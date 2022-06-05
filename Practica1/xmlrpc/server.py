d9423d18137afdae88965d6bcc5f6f72f5feb88f
deividgp <dayd4@outlook.com>
2022-06-05 12:45:05 +0200

    canvis

--- Practica1/xmlrpc/server.py
+++ Practica1/xmlrpc/server.py
@@ -12,54 +12,52 @@ WORKER_ID = 0
 llista = {}
 cua = 'cua_jobs' 
 
-def startWorker(w_id):
-    global cua
-    global r
-    
-    while True:
-        elem = r.lpop(cua)
-
-        if elem != None:
-            line = str(elem).split(':')
-            task = line[0]
-            task = task[2:]
-        
-            fitxer = line[1]
-            fitxer = fitxer[:len(fitxer)]
-            
-            cont = line[2]
-            cont = cont[2:len(cont)-1]
-
-            num = line[3]
-            num =  num[:len(num)-1]
-
-            url = 'http://localhost:8000/' + fitxer
-            os.system('curl ' + url + '> redireccio_'+ cont + num + '.txt')
-            arg1 = 'redireccio_' + cont + num + '.txt'
-            
-            result = eval(task)(arg1)
-            if (int(num) > 0):
-                time.sleep(1)
-
-                aux = r.get(cont)
-                aux = str(aux)
-                aux = aux[2:len(aux)-1]  
-                
-                result = aux + '*' + str(result)
-
-            r.mset({cont : str(result)})
-            os.system('rm ' + arg1)
-
 def createWorker():
-    global WORKERS
     global WORKER_ID
 
-    #proces = Process(target=startWorker, args=(WORKER_ID,))
-    #proces.start()
-    
-    #WORKERS[WORKER_ID] = proces
-    #WORKER_ID =  WORKER_ID+1
+    WORKER_ID =  WORKER_ID+1
     return ('WORKER CREAT = ',WORKER_ID)
+
+def deleteWorker(cont):
+    global WORKERS
+    global WORKER_ID
+    
+    proces = WORKERS[cont-1]
+    proces.terminate()
+    proces.is_alive()
+    WORKERS.pop(cont-1)
+    
+    return ('WORKER ELIMINAT= ',cont)
+
+def read_csv():
+    print("hola")
+
+def apply():
+    print("hola")
+
+def columns():
+    print("hola")
+
+def groupby():
+    print("hola")
+
+def head():
+    print("hola")
+
+def isin():
+    print("hola")
+
+def items():
+    print("hola")
+
+def apply():
+    print("hola")
+
+def max():
+    print("hola")
+
+def min():
+    print("hola")
 
 if __name__ == '__main__':
     # Set up logging
